2e77d4825e3f018aff7b784816ec1ff879de6c8e
Rhajiit <ksd9638@gmail.com>
2024-04-09 17:53:21 +0900

    ✨ Feat: share/id를 통해 다른 share폴더를 불러올 수 있도록 함

--- pages/share/[id].tsx
+++ pages/share/[id].tsx
@@ -1,25 +1,28 @@
 import { Context } from "vm";
 import Share from ".";
 import { acceptDataFromApi } from "@/src/utils/api";
+import UserDataType from "@/src/types/UserDataType";
+import { UserLinkDataType } from "@/src/types/UserLinkDataType";
+import refineLinkData from "@/src/utils/refine-link-data/refineLinkData";
 
 export async function getServerSideProps(context: Context) {
   const { id } = context.params;
   const { data } = await acceptDataFromApi(`folders/${id}`);
-  console.log(data[0]);
 
   const isNotFolder = data.length === 0;
 
   if (!isNotFolder) {
     const userId = data[0]["user_id"];
     const shareUserData = await acceptDataFromApi(`users/${userId}`);
-    const shareFolderData = await acceptDataFromApi(
+    const shareFolderLinkData = await acceptDataFromApi(
       `users/${userId}/links?folderId=${id}`,
     );
 
     return {
       props: {
-        shareUserData,
-        shareFolderData,
+        shareUserData: shareUserData["data"][0],
+        shareFolderLinkData: refineLinkData(shareFolderLinkData.data),
+        folderName: data[0]["name"],
       },
     };
   }
@@ -28,10 +31,25 @@ export async function getServerSideProps(context: Context) {
     props: {
       shareUserData: null,
       shareFolderData: null,
+      folderName: null,
     },
   };
 }
 
-export default function ShareId({ shareUserData, shareFolderData }: any) {
-  return <Share />;
+export default function ShareId({
+  shareUserData,
+  shareFolderLinkData,
+  folderName,
+}: {
+  shareUserData: UserDataType;
+  shareFolderLinkData: UserLinkDataType[];
+  folderName: string;
+}) {
+  return (
+    <Share
+      shareUserData={shareUserData}
+      shareFolderData={shareFolderLinkData}
+      folderName={folderName}
+    />
+  );
 }
