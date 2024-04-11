4310c054e55af8fb72c68845921d297779982885
Rhajiit <ksd9638@gmail.com>
2024-04-12 02:19:39 +0900

    ➕ Chore: 색상 팔레트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,16 @@ module.exports = {
     extend: {
       colors: {
         primary: "#6d6afe",
+        black: "#111322",
+        red: "#ff5b56",
+        white: "#ffffff",
+        skyblue: "#6ae3fe",
+        gray100: "#3e3e43",
+        gray60: "#9fa6b2",
+        gray20: "#ccd5e3",
+        gray10: "#e7effb",
+        lightgray: "#f5f5f5",
+        background: "#f0f6ff",
       },
       fontFamily: {
         acme: "acme",
