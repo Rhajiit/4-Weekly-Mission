57abd97d5402269b0d008fafd08b44efb9c2c0a5
Rhajiit <ksd9638@gmail.com>
2024-05-10 16:54:00 +0900

    ✨ 폴더 추가기능 완성

--- src/components/modal/add-sub-folder/AddSubFolder.tsx
+++ src/components/modal/add-sub-folder/AddSubFolder.tsx
@@ -1,16 +1,34 @@
+import { FormEvent, RefObject, useRef } from "react";
 import * as S from "../modalLoader.style";
+import { acceptDataFromApi } from "@/src/utils/api";
+import { useRouter } from "next/router";
 
 /**
  *
  * @returns 새로운 폴더를 추가하고자 할 때 input과 동시에 띄워줄 모달입니다.
  */
 export default function ModalAddSubFolder() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const handleAddSubFolder = async (e: FormEvent) => {
+    e.preventDefault();
+    if (inputRef.current!.value === "") return;
+
+    await acceptDataFromApi("folders", {
+      method: "POST",
+      body: JSON.stringify({ name: inputRef.current!.value }),
+    });
+
+    router.reload();
+  };
+
   return (
     <>
       <S.ModalTitle>폴더 추가</S.ModalTitle>
-      <form action="submit">
-        <S.ModalInput placeholder="내용 입력" type="text" />
-        <S.ModalButton>변경하기</S.ModalButton>
+      <form action="submit" onSubmit={(e: FormEvent) => handleAddSubFolder(e)}>
+        <S.ModalInput ref={inputRef} placeholder="내용 입력" type="text" />
+        <S.ModalButton>추가하기</S.ModalButton>
       </form>
     </>
   );
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,23 +4,24 @@ import { RequestInit } from "next/dist/server/web/spec-extension/request";
 const BASE_API_URL = "https://bootcamp-api.codeit.kr/api/linkbrary/v1/";
 
 /**
- * @param query baseURL 에서 추가로 요청할 정보의 경로를 적습니다.
+ * @param endPoint baseURL 에서 추가로 요청할 정보의 경로를 적습니다.
  * @param requestInformation method가 GET이 아닐 경우 추가로 작성하여 request의 성질을 조절합니다.
  * @description 서버에 대한 Request 전반을 다루는 함수입니다.
  * @returns
  */
 const acceptDataFromApi = async function (
-  query: string,
-  requestMethod?: RequestInit,
+  endPoint: string,
+  requestMethod?: any,
 ) {
   const accessToken = getCookie("accessToken");
   const method = requestMethod || { method: "GET" };
   const authorizationMethod = Object.assign(method, {
-    headers: { Authorization: accessToken },
+    headers: { Authorization: accessToken, "Content-Type": "application/json" },
   });
 
   try {
-    const response = await fetch(BASE_API_URL + query, authorizationMethod);
+    console.log(authorizationMethod);
+    const response = await fetch(BASE_API_URL + endPoint, authorizationMethod);
     if (!response.ok) {
       throw new Error("정보를 받아오는데 실패하였습니다.");
     }
